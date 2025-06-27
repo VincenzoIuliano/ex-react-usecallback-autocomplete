1db06b9037894090a21d777f58bdbb1c0f53ed7c
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-06-27 15:49:28 +0200

    Milestone 1 solved

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  console.log("suggestions", suggestions);
+
+  const fetchProducts = async (query) => {
+    if (!query) {
+      setSuggestions([]);
+      return;
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:3333/products?search=${query}`
+      );
+      const data = await response.json();
+      setSuggestions(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(query);
+  }, [query]);
+
+  return (
+    <>
+      <div>
+        <h1>Autocomplete</h1>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Cerca un prodotto..."
+        />
+        {suggestions.length > 0 && (
+          <div className="dropdown">
+            {suggestions.map((product) => (
+              <p key={product.id}>{product.name}</p>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
